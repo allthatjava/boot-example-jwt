e19881c7ca36da62daa9e456f353fb4b637937ad
Brian Heo <brian.heo@scotiabank.com>
2019-09-05 15:03:39 -0400

    Add JWT Token validation part

--- src/main/java/brian/example/boot/jwt/config/WebConfig.java
+++ src/main/java/brian/example/boot/jwt/config/WebConfig.java
@@ -1,17 +1,25 @@
 package brian.example.boot.jwt.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class WebConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private JwtRequestFilter jwtFilter;
+	@Autowired
+	private JwtAuthenticationEntryPoint entryPoint;
 	
 	@Bean
 	@Override
@@ -34,7 +42,19 @@ public class WebConfig extends WebSecurityConfigurerAdapter{
 
 		
 		http.csrf().disable()
-			.authorizeRequests().antMatchers("/authenticate").permitAll();
+			.authorizeRequests().antMatchers("/authenticate").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.exceptionHandling().authenticationEntryPoint(entryPoint)
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		
+//		anyRequest().authenticated().and().
+//		// make sure we use stateless session; session won't be used to
+//		// store user's state.
+//		exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+//		.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+		http.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
 	}
 }
